379c13f4620219c7f5abcd21638fb76682b9ff2c
Derek Alves <derek.alves@somagrupo.com.br>
2024-07-11 00:42:25 -0300

    create test to validate when api service error return errors

--- app/src/test/java/com/the_news/data/datasource/NewsRemoteDatasourceTest.kt
+++ app/src/test/java/com/the_news/data/datasource/NewsRemoteDatasourceTest.kt
@@ -3,9 +3,12 @@ package com.the_news.data.datasource
 import com.the_news.data.model.ApiResponse
 import com.the_news.data.model.Article
 import com.the_news.data.model.Source
+import com.the_news.data.util.isSuccess
 import com.the_news.infra.api.NewsAPIService
 import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.runBlocking
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.ResponseBody.Companion.toResponseBody
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito.mock
@@ -51,6 +54,23 @@ class NewsRemoteDatasourceTest {
 
             val response = sut.getTopHeadlines(page = 1, category = "any-category")
             assertEquals(response.data?.status, "ok")
+            assertEquals(response.data?.articles, articlesMock)
+            assertEquals(response.data?.totalResults, 10)
+        }
+    }
+
+    @Test
+    fun getTopHeadlines_returnAnDifferentSuccess() {
+        runBlocking {
+            val errorBody =
+                "Your error message".toResponseBody("application/json".toMediaTypeOrNull())
+
+            `when`(apiServiceMock.getTopHeadlines(page = 1, category = "any-category"))
+                .thenReturn(Response.error(500, errorBody))
+
+            val response = sut.getTopHeadlines(page = 1, category = "any-category")
+            assertEquals(response.isSuccess(), false)
+            assertEquals(response.code, 500)
         }
     }
 }
